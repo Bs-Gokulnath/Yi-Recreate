e49457242eb289983460738f7df7e29f52790414
gokulnath_bs <bs.gokulnath18@gmail.com>
2025-06-24 16:14:50 +0530

    home and navbar added

--- Yi/Yi-Recreate1/src/App.jsx
+++ Yi/Yi-Recreate1/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import CustomCursor from './components/CustomCursor';
+import LandingPage from './pages/Homepage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <CustomCursor />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
